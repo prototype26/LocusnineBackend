08c62f4b404d78c7b7128a42a041f9e46d70d752
Mohammad Akbar <akbardastgeer@gmail.com>
2020-03-07 15:49:13 +0530

    post working

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-//get--/locusnineapi/v1/users
-// app.get('/locusnineapi/v1/users',(req, res)=>{
-//     axios.get('https://locusninedevdata.firebaseio.com/users.json').then(resp=>{
-//         res.status(200).json({
-//             status: 'success',
-//             results: resp.data.length,
-//             data: {
-//                 users: resp.data
-//             }
-//         })
-//     }).catch(error=>{
-//         console.log(`Error fetching data:: ${error}`);
-//         return null;
-//     });
-// });
+// get--/locusnineapi/v1/users
+app.get('/locusnineapi/v1/users',(req, res)=>{
+    axios.get('https://locusninedevdata.firebaseio.com/users.json').then(resp=>{
+        res.status(200).json({
+            status: 'success',
+            results: resp.data.length,
+            data: {
+                users: resp.data
+            }
+        })
+    }).catch(error=>{
+        console.log(`Error fetching data:: ${error}`);
+        return null;
+    });
+});
+
 //post--/locusnineapi/v1/users
 //axios post request
-
-// axios.post('https://locusninedevdata.firebaseio.com/users.json',req.body)
-    //      .then(response=>console.log(`Data added successfully:: ${response.data}`))
-    //      .catch(error=>{console.log(`Failed to add data:: ${error}`)}); 
-    //      res.send('Data added.')
-app.post('/locusnine/v1/users',(req,res)=>{
-    console.log(req.body);
+app.post('/locusnineapi/v1/users',(req,res)=>{
+    axios.post('https://locusninedevdata.firebaseio.com/users.json',req.body)
+         .then(response=>console.log(`Data added successfully:: ${response.data}`))
+         .catch(error=>{console.log(`Failed to add data:: ${error}`)}); 
+         res.send('Data added.')
     res.send('Done');
 });
 
